d7dfe3d28cb276d3053baa1fc9df86453bda2ee7
Nguyễn Hải Triều <36495153+Czer0C@users.noreply.github.com>
2021-04-21 23:08:25 +0700

    Layou, header, and footer

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,7 +1,15 @@
+import { useRouter } from "next/router";
+import Layout from "../../components/Layout";
+
 export default function EventPage() {
+  const router = useRouter();
+
+  console.log({ router });
+
   return (
-    <div>
-      <h1>My Event</h1>
-    </div>
+    <Layout title={`Event ${router.query.slug}`}>
+      <h1>My Event: {JSON.stringify(router.query.slug)}</h1>
+      <button onClick={() => router.push("/")}>Push home</button>
+    </Layout>
   );
 }
