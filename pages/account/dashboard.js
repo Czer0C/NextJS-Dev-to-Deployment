47c557d63a55596ad0485eec4485cba267f8024b
Nguyễn Hải Triều <36495153+Czer0C@users.noreply.github.com>
2021-04-24 16:50:02 +0700

    dashboard item

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,5 +1,44 @@
-import Layout from "@/components/Layout";
+import { parseCookies } from "@/helpers/helpers";
 
-export default function DashboardPage() {
-  return <Layout>Dashboard Page</Layout>;
+import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
+
+import styles from "@/styles/Dashboard.module.css";
+import DashboardEvent from "@/components/DashboardEvent";
+
+export default function DashboardPage({ evts }) {
+  const deleteEvent = (evtId) => {
+    console.log(evtId);
+  };
+  return (
+    <Layout title="User Dashboard">
+      <div className={styles.dash}>
+        <h1>Dashboard Page</h1>
+        <h3>My Events</h3>
+
+        {evts.map((evt) => (
+          <DashboardEvent key={evt.id} evt={evt} handleDelete={deleteEvent} />
+        ))}
+      </div>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookies(req);
+
+  const res = await fetch(`${API_URL}/events/me`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const evts = await res.json();
+
+  return {
+    props: {
+      evts,
+    },
+  };
 }
